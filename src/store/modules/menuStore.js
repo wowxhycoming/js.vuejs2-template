0c0cb1e4544cc791955974f0ee89f78a4e6e493b
wowxhycoming <wowxhycoming@sina.com>
2017-11-08 14:39:41 +0800

    1. 防止请求回来的菜单不合理，导致不能拼接成菜单数组，最终死循环。

--- src/store/modules/menuStore.js
+++ src/store/modules/menuStore.js
@@ -1,23 +1,27 @@
 import types from '../types';
 import {signin, getMenuList} from '@/api/authApi';
 import {constantRouterMap, asyncRouterMap, notFoundRouterMap} from '@/router';
-import {levelMatch,verifyToken} from '@/utils';
+import {levelMatch, verifyToken} from '@/utils';
 
-const menu ={
+const menu = {
     state: {
-        menuList:[],
-        asyncPermissionRouterList:[],
-        allPermissionRouterList:[]
+        menuList: [],
+        asyncPermissionRouterList: [],
+        allPermissionRouterList: []
     },
-    actions:{
+    actions: {
         [types.VX_GET_MENU]: ({commit}, token) => {
             return new Promise((resolve, reject) => {
                 getMenuList(token).then(res => {
-                    const menuList = res.data;
-                        console.log('VX_GET_MENU action menuList', menuList);
+                    const data = res.data;
+                    let menuList = data.data;
+                    if(!(menuList instanceof Array)) {
+                        menuList = []; // 避免组装菜单失败，无限请求获取菜单接口
+                    }
+                    console.log('VX_GET_MENU action menuList', menuList);
                     const asyncPermissionRouterList = levelMatch(menuList, asyncRouterMap).concat(notFoundRouterMap);
-                        console.log('VX_GET_MENU action asyncPermissionRouterList', asyncPermissionRouterList);
-                    commit(types.VX_GET_MENU, {menuList,asyncPermissionRouterList});
+                    console.log('VX_GET_MENU action asyncPermissionRouterList', asyncPermissionRouterList);
+                    commit(types.VX_GET_MENU, {menuList, asyncPermissionRouterList});
                     resolve();
                 }).catch(err => {
                     reject(err);
